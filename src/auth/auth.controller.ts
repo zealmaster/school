07923efef2a7a0c32656485017b6af048aee5916
zealmaster <64289212+zealmaster@users.noreply.github.com>
2023-01-15 13:11:46 +0100

    Made changes to /auth/login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,15 +7,16 @@ import {
   Post,
 } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/guards/local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    const user = req.user;
-    return user;
+    return this.authService.login(req.user);
   }
 
   @Get('logout')
