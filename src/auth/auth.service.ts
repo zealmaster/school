a4d16595ceb660a7d0937333a44736a020bb9d8c
zealmaster <64289212+zealmaster@users.noreply.github.com>
2023-01-15 13:40:21 +0100

    Modified  validateUser method. Create login()
    Modified  validateUser method. Create login(). Add method for JWT strategy implementation.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { loginDto } from 'src/dto/login.dto';
 import { UserService } from 'src/user/user.service';
-
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(username: string, password: string) {
-    const user = await this.userService.findOne({ username });
-    if (user && user.password === password) {
-      return user;
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (!user) {
+      return 'Incorrect username';
     }
-    return null;
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (user && isMatch) {
+      return user.username + ' is a correct user.';
+    }
+    return 'password incorrect';
+  }
+
+  async login(user: any) {
+    return user;
+  }
+
+  // For Jwt-strategy
+  async loginUser(user: any) {
+    return user;
+    // if (user) {
+    //   return (
+    //     user.username +
+    //     ',You are successfully logged in. You can now add your school details.'
+    //   );
+    // }
+    // const payload = {
+    //   user: {
+    //     id: user.user.id,
+    //     email: user.user.email,
+    //     name: user.user.username,
+    //   },
+    // };
+    // return await {
+    //   access_token: this.jwtService.sign(payload),
+    // };
   }
 }
